90be1df40b44e572edb69965af292fa9d75b95b3
Aryel Mota Góis <aryel.gois@gmail.com>
2017-10-09 09:46:49 -0300

    Fix refresh problem (again)
    The last part of URI was being lost

--- src/Controllers/PageLogin.php
+++ src/Controllers/PageLogin.php
@@ -19,7 +19,7 @@ class PageLogin
         } else {
             $form = new App\Models\Forms\Login($_POST);
             if ($form->checkLogin()) {
-                header('Location: .');
+                header('Location: ' . $_SERVER['REQUEST_URI']);
                 die();
             } else {
                 $view = new App\Views\PageLogin($form);
